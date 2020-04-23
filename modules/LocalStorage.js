2744af0aead61b098d9add57b35c685e9a968250
indre101 <zygaityte.indre@gmail.com>
2020-04-23 13:55:29 +0200

    adds a local storage function

---
+++ modules/LocalStorage.js
@@ -0,0 +1,16 @@
+localStorage.setItem("isSubscribed", "false");
+
+document.querySelector("button").onclick = function () {
+  localStorage.setItem("isSubscribed", "false");
+  const issubscirbed = localStorage.getItem("isSubscribed");
+
+  showUser(issubscirbed);
+};
+
+function showUser(issubscirbed) {
+  if (issubscirbed === "true") {
+    console.log("sub");
+  } else if (issubscirbed === "false") {
+    console.log("notusb");
+  }
+}
