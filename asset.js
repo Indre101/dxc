95c6fa804fe5bccf9b98e7169740c5f54ab4c467
Graeme Clark <grae0026@stud.kea.dk>
2020-04-29 14:51:00 +0200

    fix hexagon svg

--- asset.js
+++ asset.js
@@ -2,10 +2,16 @@ import { url, apiKey } from "./modules/urlKey";
 
 import { intersector } from "./modules/intersectionAnim";
 import { manipulateEntry } from "./modules/Postingdata";
+import { getSVG } from "./modules/svgModule";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+	duration: 1200,
+});
 
 intersector();
-
-
+getSVG();
 
 const bodyScrollLock = require("body-scroll-lock");
 const disableBodyScroll = bodyScrollLock.disableBodyScroll;
@@ -14,114 +20,100 @@ const enableBodyScroll = bodyScrollLock.enableBodyScroll;
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  getCustomerData();
-  closeModal();
+	getCustomerData();
+	closeModal();
 
-  document.querySelector("form").addEventListener("submit", (event) => {
-    event.preventDefault();
-    const updatedInfom = addNewData();
-    manipulateEntry(updatedInfom);
-    document.querySelector(".page-wrapper").dataset.active = "";
-    document.querySelector(".modal").dataset.active = "false";
-    const targetElement = document.querySelector("#page-wrapper");
-    enableBodyScroll(targetElement);
-  });
+	document.querySelector("form").addEventListener("submit", (event) => {
+		event.preventDefault();
+		const updatedInfom = addNewData();
+		manipulateEntry(updatedInfom);
+		document.querySelector(".page-wrapper").dataset.active = "";
+		document.querySelector(".modal").dataset.active = "false";
+		const targetElement = document.querySelector("#page-wrapper");
+		enableBodyScroll(targetElement);
+	});
 }
 
 async function getCustomerData() {
-  const customerId = localStorage.getItem("customerid");
-  const data = await fetch(`${url}/${customerId}`, {
-    method: "get",
-    headers: {
-      "Content-type": "application/json; charset=utf-8",
-      "x-apikey": apiKey,
-      "cache-control": "no-cache",
-    },
-  });
-  const response = await data.json();
-  manipulateEntry(response);
-  checkIfsubscirbed(response);
-
+	const customerId = localStorage.getItem("customerid");
+	const data = await fetch(`${url}/${customerId}`, {
+		method: "get",
+		headers: {
+			"Content-type": "application/json; charset=utf-8",
+			"x-apikey": apiKey,
+			"cache-control": "no-cache",
+		},
+	});
+	const response = await data.json();
+	manipulateEntry(response);
+	checkIfsubscirbed(response);
 }
 
 const options = {
-  treshold: 1,
-  rootMargin: "0px 0px -50px 0px",
+	treshold: 1,
+	rootMargin: "0px 0px -50px 0px",
 };
 
-const modalObserver = new IntersectionObserver(function (
-  entries,
-  modalObserver
-) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    } else if (entry.isIntersecting) {
-      showModal();
-      modalObserver.unobserve(entry.target);
-    }
-  });
-},
-options);
+const modalObserver = new IntersectionObserver(function (entries, modalObserver) {
+	entries.forEach((entry) => {
+		if (!entry.isIntersecting) {
+			return;
+		} else if (entry.isIntersecting) {
+			showModal();
+			modalObserver.unobserve(entry.target);
+		}
+	});
+}, options);
 
 function checkIfsubscirbed(item) {
-
-  if (!item.subscriber && item.visitcount > 1) {
-    modalObserver.observe(document.querySelector(".aboutAuthor"));
-  } else {
-    document.querySelector(".modal").dataset.active = "false";
-  }
+	if (!item.subscriber && item.visitcount > 1) {
+		modalObserver.observe(document.querySelector(".aboutAuthor"));
+	} else {
+		document.querySelector(".modal").dataset.active = "false";
+	}
 }
 
 function showModal() {
-  console.log("modal");
-  document.querySelector(".modal").dataset.active = "";
-  const targetElement = document.querySelector("#page-wrapper");
-  disableBodyScroll(targetElement);
+	console.log("modal");
+	document.querySelector(".modal").dataset.active = "";
+	const targetElement = document.querySelector("#page-wrapper");
+	disableBodyScroll(targetElement);
 }
 
 function closeModal() {
-  document.querySelector(".closeBtn").addEventListener("click", () => {
-    document.querySelector(".modal").dataset.active = "false";
-    document.querySelector(".page-wrapper").dataset.active = "";
-    const targetElement = document.querySelector("#page-wrapper");
-    enableBodyScroll(targetElement);
-  });
+	document.querySelector(".closeBtn").addEventListener("click", () => {
+		document.querySelector(".modal").dataset.active = "false";
+		document.querySelector(".page-wrapper").dataset.active = "";
+		const targetElement = document.querySelector("#page-wrapper");
+		enableBodyScroll(targetElement);
+	});
 }
 
 function addNewData() {
-  const {
-    firstName,
-    lastName,
-    workemail,
-    isSubscribed,
-  } = getCustomerDataFromForm();
+	const { firstName, lastName, workemail, isSubscribed } = getCustomerDataFromForm();
 
-  return {
-    firstname: firstName,
-    lastname: lastName,
-    workemail: workemail,
-    subscriber: isSubscribed,
-  };
+	return {
+		firstname: firstName,
+		lastname: lastName,
+		workemail: workemail,
+		subscriber: isSubscribed,
+	};
 }
 
 function getCustomerDataFromForm() {
-  const fullName = document.querySelector("#fullName").value;
-  const fullNameSplit = fullName.split(" ").filter((word) => word != "");
-  const firstName = fullNameSplit[0];
-  const lastName =
-    fullNameSplit.length > 1
-      ? fullNameSplit.splice(1, fullNameSplit.length).join(" ")
-      : "Did not provide";
-  const workemail = document.querySelector("#workEmail").value;
-  const subscribeCheckbox = document.querySelector(".subscribe");
-  const isSubscribed = subscribeCheckbox.checked ? true : false;
+	const fullName = document.querySelector("#fullName").value;
+	const fullNameSplit = fullName.split(" ").filter((word) => word != "");
+	const firstName = fullNameSplit[0];
+	const lastName =
+		fullNameSplit.length > 1 ? fullNameSplit.splice(1, fullNameSplit.length).join(" ") : "Did not provide";
+	const workemail = document.querySelector("#workEmail").value;
+	const subscribeCheckbox = document.querySelector(".subscribe");
+	const isSubscribed = subscribeCheckbox.checked ? true : false;
 
-  return {
-    firstName,
-    lastName,
-    workemail,
-    isSubscribed,
-  };
-
+	return {
+		firstName,
+		lastName,
+		workemail,
+		isSubscribed,
+	};
 }
--- modules/svgModule.js
+++ modules/svgModule.js
@@ -3,7 +3,7 @@ import { gsap } from "gsap";
 // load svg
 async function getSVG() {
 	console.log("start");
-	let response = await fetch("images/infographics/hexagon.svg");
+	let response = await fetch("images/infographics/hexagon_with_text.svg");
 	let mySvgData = await response.text();
 	document.querySelector(".diagram").innerHTML = mySvgData;
 	console.log(mySvgData);
@@ -16,7 +16,7 @@ function fillHexagons() {
 	document.querySelectorAll("#top, #right, #rightDown, #down, #leftDown, #left, #middle, #bottom").forEach((e) => {
 		e.classList.add("yellowFill");
 	});
-	gsap.from(".yellowFill", { delay: 3, duration: 1, y: 50, opacity: 0, stagger: 0.5 });
+	gsap.from(".yellowFill", { delay: 1, duration: 1, z: 50, opacity: 0, stagger: 0.5 });
 }
 
 export { getSVG };
